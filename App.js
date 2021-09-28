054061e44cb2b603ac1845e6f10b30ea781396ea
zorro901 <zoro927@hotmail.com>
2021-09-28 21:16:19 +0900

    34. FlatListで一覧を表示する

--- App.js
+++ App.js
@@ -1,60 +1,29 @@
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
-import { StyleSheet, Text, View, Image } from 'react-native'
+import { StyleSheet, View, FlatList, SafeAreaView } from 'react-native'
+import ListItems from './components/ListItems'
+import articles from "./dammies/articles.json"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View
-        style={styles.itemContainer}>
-        <View style={styles.leftContainer}>
-          <Image
-            style={{ width: 100, height: 100 }}
-            source={{ uri: "https://picsum.photos/id/10/200/200" }}/>
-        </View>
-        <View style={styles.rightContainer}>
-          <Text numberOfLines={3} style={styles.text}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-            ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-            anim id est laborum.
-          </Text>
-          <Text style={styles.subText}>ReactNews</Text>
-        </View>
-      </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList data={articles} renderItem={({ item }) => (
+        <ListItems
+          imageUrl={item.urlToImage}
+          title={item.title}
+          author={item.author}
+        />
+      )}
+                keyExtractor={(item, index) => index.toString()}
+      />
       <StatusBar style="auto"/>
-    </View>
+    </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  itemContainer: {
-    height: 100,
-    width: '100%',
-    borderColor: "gray",
-    borderWidth: 1,
-    flexDirection: "row"
-  },
-  leftContainer: {
-    width: 100
-  },
-  rightContainer: {
-    flex: 1,
-    padding: 10,
-    justifyContent: "space-between"
-  },
-  text: {
-    fontSize: 16
-  },
-  subText: {
-    fontSize: 12,
-    color: "gray"
+    backgroundColor: '#fff'
   }
 })
--- components/ListItems.js
+++ components/ListItems.js
@@ -1,14 +1,7 @@
 import React from 'react'
 import { StyleSheet, Text, View, Image } from 'react-native'
 
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
   itemContainer: {
     height: 100,
     width: '100%',
@@ -33,3 +26,25 @@ const styles = StyleSheet.create({
   }
 })
 
+const ListItem = ({ imageUrl, title, author }) => {
+  return (
+    
+    <View
+      style={styles.itemContainer}>
+      <View style={styles.leftContainer}>
+        <Image
+          style={{ width: 100, height: 100 }}
+          source={{ uri: imageUrl }}/>
+      </View>
+      <View style={styles.rightContainer}>
+        <Text numberOfLines={3} style={styles.text}>
+          {title}
+        
+        </Text>
+        <Text style={styles.subText}>{author}</Text>
+      </View>
+    </View>
+  )
+}
+
+export default ListItem
