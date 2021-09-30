dd577608eb47c0df9614d621e44c14d455fe8f0f
zorro901 <zoro927@hotmail.com>
2021-09-30 22:27:48 +0900

    67. Reduxを永続化して端末に保存する

--- store/index.js
+++ store/index.js
@@ -1,5 +1,7 @@
 import { createStore, combineReducers } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import { persistReducer, persistStore } from 'redux-persist'
+import { AsyncStorage } from 'react-native'
 
 import userReducer from './reducers/user'
 
@@ -7,6 +9,13 @@ const rootReducer = combineReducers({
   user: userReducer
 })
 
-const store = createStore(rootReducer, composeWithDevTools())
+const persistConfig = {
+  key: "root",
+  storage: AsyncStorage,
+  whitelist: ["user"]
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+const store = createStore(persistedReducer, composeWithDevTools())
 
+export const persistor = persistStore(store)
 export default store
